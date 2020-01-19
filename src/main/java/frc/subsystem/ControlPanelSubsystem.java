b3bc20a08257967d15e54484096370ba64fc1095
Noah Connors <noahconnors@mnmsa.org>
2020-01-18 18:08:24 -0600

    Add some color functionality to Control Panel Subsystem

--- src/main/java/frc/subsystem/ControlPanelSubsystem.java
+++ src/main/java/frc/subsystem/ControlPanelSubsystem.java
@@ -1,5 +1,8 @@
 package frc.subsystem;
 
+import com.revrobotics.ColorSensorV3;
+
+import edu.wpi.first.wpilibj.I2C;
 import frc.MotorWrapper;
 import frc.ServiceLocator;
 import frc.info.RobotInfo;
@@ -8,10 +11,17 @@ public class ControlPanelSubsystem {
 
     private final RobotInfo robotInfo;
     private final MotorWrapper controlPanelMotor;
+    private final ColorSensorV3 colorSensor; 
+    public static double[] controlPanelCyan = {0, 255, 255};
+    public static double[] controlPanelGreen = {0, 255, 0};
+    public static double[] controlPanelRed = {255, 0, 0};
+    public static double[] controlPanelYellow = {255, 255, 0};
+
 
     public ControlPanelSubsystem() {
         robotInfo = ServiceLocator.get(RobotInfo.class);
         controlPanelMotor = robotInfo.get(RobotInfo.CONTROL_PANEL_MOTOR);
+        colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
     }
 
     public void spinControlPanel() {
@@ -22,4 +32,54 @@ public class ControlPanelSubsystem {
         controlPanelMotor.set(0);
     }
 
+    public double getColorSensorRed() {
+        return colorSensor.getColor().red;
+    } 
+
+    public double getColorSensorBlue() {
+        return colorSensor.getColor().blue;
+    }
+
+    public double getColorSensorGreen() {
+        return colorSensor.getColor().green;
+    }
+
+    public double getColorSensorIR() {
+        return colorSensor.getIR();
+    }
+
+    public double getColorSensorProximity() {
+        return colorSensor.getProximity();
+    }
+
+    public String getControlPanelColor(double red, double green, double blue) {
+
+    }
+
+    public static double getHue(double red, double green, double blue) {
+        double cMax = Math.max(Math.max(red, green), blue);
+        double cMin = Math.min(Math.min(red, green), blue);
+        double delta = cMax - cMin;
+        System.out.println("cMax = " + cMax + " cMin = " + cMin + " delta = " + delta);
+    
+        if (delta == 0) {
+          System.out.println("0");
+          return 0;
+        } else if (cMax == red) {
+          System.out.println("red");
+          return 60 * mod(((green - blue) / delta), 6);
+        } else if (cMax == green) {
+          System.out.println("green");
+          return 60 * (((blue - red) / delta) + 2);
+        } else if (cMax == blue) {
+          System.out.println("blue");
+          return 60 * (((red - green) / delta) + 4);
+        } else {
+          return 0;
+        }
+      } 
+
+      public static double mod(double a, double b) {
+        return ((a % b) + b) % b;
+      }
 }
