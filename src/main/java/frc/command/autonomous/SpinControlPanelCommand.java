15a66d2d6cbb17d6e91acc47dd2b2c97e38779b1
Karina <karinamdovgodko@gmail.com>
2020-01-23 20:30:34 -0600

    Fix spin control panel command

--- src/main/java/frc/command/autonomous/SpinControlPanelCommand.java
+++ src/main/java/frc/command/autonomous/SpinControlPanelCommand.java
@@ -10,10 +10,10 @@ public class SpinControlPanelCommand extends Command {
     private ControlPanelSubsystem controlPanelSubsystem;
     private String goalColor;
     private String currentColor;
-    private double[] redAngles = { 22.5, 202.5 };
-    private double[] greenAngles = { 67.5, 247.5 };
-    private double[] blueAngles = { 112.5, 292.5 };
-    private double[] yellowAngles = { 157.5, 337.5 };
+    private static double[] redAngles = { 22.5, 202.5 };
+    private static double[] greenAngles = { 67.5, 247.5 };
+    private static double[] blueAngles = { 112.5, 292.5 };
+    private static double[] yellowAngles = { 157.5, 337.5 };
 
     public SpinControlPanelCommand(String goalColor) {
         controlPanelSubsystem = ServiceLocator.get(ControlPanelSubsystem.class);
@@ -28,44 +28,7 @@ public class SpinControlPanelCommand extends Command {
         currentColor = ControlPanelSubsystem
             .getControlPanelColor(ControlPanelSubsystem.getHue(controlPanelSubsystem.getColorSensorRed(),
             controlPanelSubsystem.getColorSensorGreen(), controlPanelSubsystem.getColorSensorBlue()));
-        double currentAngle;
-        if (currentColor == "red") {
-            currentAngle = blueAngles[0];
-        }
-        if (currentColor == "green")` {
-            currentAngle = yellowAngles[0];
-        }
-        if (currentColor == "blue") {
-            currentAngle = redAngles[0];
-        }
-        if (currentColor == "yellow") {
-            currentAngle = greenAngles[0];
-        } else {
-            currentAngle = 0;
-        }
-
-        double[] goalAngles;
-        if (goalColor == "red") {
-            goalAngles = redAngles;
-        } else if (goalColor == "green") {
-            goalAngles = greenAngles;
-        } else if (goalColor == "blue") {
-            goalAngles = blueAngles;
-        } else if (goalColor == "yellow") {
-            goalAngles = yellowAngles;
-        } else {
-            goalAngles = new double[] { 0, 0 };
-        }
-        double distance1 = MathUtility.getDistanceBetweenAngles(currentAngle, goalAngles[0]);
-        double distance2 = MathUtility.getDistanceBetweenAngles(currentAngle, goalAngles[1]);
-
-        double shortestDistance;
-        if (Math.abs(distance1) < Math.abs(distance2)) {
-            shortestDistance = distance1;
-        } else {
-            shortestDistance = distance2;
-        }
-
+        double shortestDistance = getShortestDistance(currentColor, goalColor);
         if (shortestDistance > 0) {
             controlPanelSubsystem.spinControlPanelForward();
         } else {
@@ -81,5 +44,44 @@ public class SpinControlPanelCommand extends Command {
     public void end() {
         controlPanelSubsystem.stopSpinControlPanel();
 
+    }
+
+    public static double getShortestDistance(String currentColor, String goalColor) {
+        double currentAngle;
+        if (currentColor.equals("red")) {
+            currentAngle = blueAngles[0];
+        } else if (currentColor.equals("green")) {
+            currentAngle = yellowAngles[0];
+        } else if (currentColor.equals("blue")) {
+            currentAngle = redAngles[0];
+        } else if (currentColor.equals("yellow")) {
+            currentAngle = greenAngles[0];
+        } else {
+            currentAngle = 0;
+            System.out.print("current angle not set");
+        }
+
+        double[] goalAngles;
+        if (goalColor.equals("red")) {
+            goalAngles = redAngles;
+        } else if (goalColor.equals("green")) {
+            goalAngles = greenAngles;
+        } else if (goalColor.equals("blue")) {
+            goalAngles = blueAngles;
+        } else if (goalColor.equals("yellow")) {
+            goalAngles = yellowAngles;
+        } else {
+            goalAngles = new double[] { 0, 0 };
+        }
+        double distance1 = MathUtility.getDistanceBetweenAngles(currentAngle, goalAngles[0]);
+        double distance2 = MathUtility.getDistanceBetweenAngles(currentAngle, goalAngles[1]);
+
+        double shortestDistance;
+        if (Math.abs(distance1) < Math.abs(distance2)) {
+            shortestDistance = distance1;
+        } else {
+            shortestDistance = distance2;
+        }
+        return shortestDistance;
     }
 }
