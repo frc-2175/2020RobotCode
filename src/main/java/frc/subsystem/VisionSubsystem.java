170b72b25077da475f3d9602abb75d5bbfbec9cf
Karina <karinamdovgodko@gmail.com>
2020-01-25 15:22:23 -0600

    Add vision subsystem

---
+++ src/main/java/frc/subsystem/VisionSubsystem.java
@@ -0,0 +1,25 @@
+package frc.subsystem;
+
+import frc.Vector;
+
+public class VisionSubsystem {
+    private static final double cameraAngle = Math.toRadians(20);
+    private static final double cameraHeight = 10;
+    private static final double targetWidth = 39.25;
+    private static final double targetHeight = 17;
+    private static final double targetBottomHeightFromFloor = 81.25;
+    private static final double targetTopHeightFromFloor = targetBottomHeightFromFloor + targetHeight;
+
+
+    public double getDistanceFromTarget(double pixelAngleY) {
+        double angle = pixelAngleY + cameraAngle;
+        return (targetHeight - cameraHeight) / Math.tan(angle);
+    }
+
+    public Vector getTargetPositionInCameraSpace(double pixelAngleX, double pixelAngleY) {
+        double distance = getDistanceFromTarget(pixelAngleY);
+        double yPosition = Math.sin(pixelAngleX) * distance;
+        double xPosition = Math.cos(pixelAngleX) * distance;
+        return new Vector(xPosition, yPosition);
+    }
+}
