f0261600d386d96f0c34e7c2b44260239a68015e
Ben Visness <bvisness@gmail.com>
2020-01-25 11:45:49 -0600

    Try without shell=True now that we are actually selecting the bat

--- benkins.py
+++ benkins.py
@@ -14,11 +14,11 @@ if platform.system() == "Windows":
     gradle = "gradlew.bat"
 
 try:
-    subprocess.run([gradle, "build"], check=True, shell=True)
+    subprocess.run([gradle, "build"], check=True)
 except subprocess.CalledProcessError as e:
     fail("Failed to compile code. See the logs for more details.")
 
 try:
-    subprocess.run([gradle, "test"], check=True, shell=True)
+    subprocess.run([gradle, "test"], check=True)
 except subprocess.CalledProcessError as e:
     fail("Tests failed. See the logs for more details.")
