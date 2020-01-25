4ad4f6c19a3800cdf9aa45fad0b01657c973c639
Ben Visness <bvisness@gmail.com>
2020-01-24 21:55:53 -0600

    Print better things
    Revert unwanted "fixes"

--- benkins.py
+++ benkins.py
@@ -4,6 +4,7 @@ import platform
 import subprocess
 
 def fail(msg):
+    print(msg)
     with open('benkins-notification.txt', 'w') as f:
         f.write(msg)
     exit(1)
@@ -13,13 +14,11 @@ if platform.system == "Windows":
     gradle = "gradlew.bat"
 
 try:
-    result = subprocess.run([gradle, "build"], check=True)
-    print(result)
+    subprocess.run([gradle, "build"], check=True)
 except subprocess.CalledProcessError as e:
     fail("Failed to compile code. See the logs for more details.")
 
 try:
-    result = subprocess.run([gradle, "test"], check=True)
-    print(result)
+    subprocess.run([gradle, "test"], check=True)
 except subprocess.CalledProcessError as e:
     fail("Tests failed. See the logs for more details.")
