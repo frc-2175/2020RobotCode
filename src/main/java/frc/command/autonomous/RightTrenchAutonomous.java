e6dd9c4189de93e176ca426267467682ed49a760
theok15 <theokroells@gmail.com>
2020-01-24 19:36:15 -0600

    Add Autonomous code files for latter.

---
+++ src/main/java/frc/command/autonomous/RightTrenchAutonomous.java
@@ -0,0 +1,10 @@
+package frc.command.autonomous;
+
+import edu.wpi.first.wpilibj.Timer;
+import frc.ServiceLocator;
+import frc.command.Command;
+import frc.subsystem.IntakeSubsystem;
+
+public class RightTrenchAutonomous extends Command {
+
+}
