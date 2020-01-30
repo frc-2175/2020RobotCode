3baf4335ed900717b66010c9b1ec9c607f14f605
Ben Visness <bvisness@gmail.com>
2020-01-29 21:04:19 -0600

    Change the wait script

--- benkins-deploy-wait.py
+++ benkins-deploy-wait.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import netconsole
+import sys
+
+print("Waiting for robot to boot...")
 
 def customPrintFn(s):
-    netconsole._output_fn(s)
+    sys.stdout.write(s.encode(sys.stdout.encoding, errors='replace').decode(sys.stdout.encoding))
+    sys.stdout.write('\n')
     if "disabled and ready" in s:
+        console.stop()
         exit(0)
 
 console = netconsole.Netconsole(customPrintFn)
