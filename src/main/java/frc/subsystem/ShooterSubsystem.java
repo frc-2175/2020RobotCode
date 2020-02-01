6b1fd2a5c91d6064ca54613655b5dabf0f1e48e9
Nupur <kumarnupurk@gmail.com>
2020-01-31 19:56:49 -0600

    Continue with turret code

--- src/main/java/frc/subsystem/ShooterSubsystem.java
+++ src/main/java/frc/subsystem/ShooterSubsystem.java
@@ -12,26 +12,34 @@ public class ShooterSubsystem {
     private final MotorWrapper shooterMotorMaster;
     private final MotorWrapper shooterMotorFollower;
     private final MotorWrapper turretMotor;
-    private final PIDController pidController; 
-    double conversionNumber = 4096;
+    private final PIDController shooterWheelPidController; 
+    private final PIDController turretPidController;
     public enum Mode {Manual, PID, BangBang};
     private Mode currentMode = Mode.Manual;
     private double goalSpeedRPM;
     private double manualSpeed = 0;
     private double speedThreshold = 5;
     private double speedRange = 5;
-    
+    private double goalAngle;
+    private double degreesConversionNumber = 1; //!!!!!!! not accurate
+    private double conversionNumber = 4096;
+
+
 
     public ShooterSubsystem() {
         robotInfo = ServiceLocator.get(RobotInfo.class);
         shooterMotorMaster = robotInfo.get(RobotInfo.SHOOTER_MOTOR_MASTER);
         shooterMotorFollower = robotInfo.get(RobotInfo.SHOOTER_MOTOR_FOLLOWER);
         turretMotor = robotInfo.get(RobotInfo.TURRET_MOTOR);
-        double kp = 1.0 / 36.0;
-        double ki = 1.0 / 30.0;
-        double kd = 0;
+        double sp = 1.0 / 36.0;
+        double si = 1.0 / 30.0;
+        double sd = 0;
+        double ti = 1;
+        double tp = 1;
+        double td = 1;
         
-        pidController = new PIDController(kp, ki, kd);
+        shooterWheelPidController = new PIDController(sp, si, sd);
+        turretPidController = new PIDController(tp, ti, td);
         
         shooterMotorFollower.follow(shooterMotorMaster);
 
@@ -41,7 +49,7 @@ public class ShooterSubsystem {
 
     public void periodic() {
         if (currentMode == Mode.PID) { //PID!!
-            shooterMotorMaster.set(shooterMotorMaster.get() + pidController.pid(getSpeedInRPM(), goalSpeedRPM));
+            shooterMotorMaster.set(shooterMotorMaster.get() + shooterWheelPidController.pid(getSpeedInRPM(), goalSpeedRPM));
         } else if (currentMode == Mode.BangBang) { //Bang bang !!!!!
             if ( getSpeedInRPM() >  (speedThreshold + speedRange)) {
                 shooterMotorMaster.set(0);
@@ -78,6 +86,15 @@ public class ShooterSubsystem {
 
     public void setManualSpeed(double jacob) {
         manualSpeed = jacob;
+    }
+
+    public void setAnglePID() {
+        turretMotor.set(turretPidController.pid(turretMotor.getSelectedSensorPosition(0), goalAngle));
+    }
+
+    public double getTurretAngle() {
+        return 0;
+        //TODO !!!!!!!!!!!!!!!!!!!!!!!!
     }
 }
 
