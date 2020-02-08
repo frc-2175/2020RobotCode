f28ce42d94ba495e23db24abecd60dfcc497bcd1
Karina <karinamdovgodko@gmail.com>
2020-02-08 12:46:45 -0600

    Comment vision directions

--- src/main/java/frc/subsystem/VisionSubsystem.java
+++ src/main/java/frc/subsystem/VisionSubsystem.java
@@ -18,8 +18,8 @@ public class VisionSubsystem {
 
     public Vector getPixelPositionInCameraSpace(double pixelAngleX, double pixelAngleY, double height) {
         double distance = getDistanceFromTarget(pixelAngleY, height);
-        double yPosition = Math.sin(pixelAngleX) * distance;
-        double xPosition = Math.cos(pixelAngleX) * distance;
+        double yPosition = Math.sin(pixelAngleX) * distance; //parallel to target
+        double xPosition = Math.cos(pixelAngleX) * distance; //perpendicular to target 
         return new Vector(xPosition, yPosition);
     }
 
