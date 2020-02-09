45d49b065836e8c2600d028f8fd8c2880adb0067
theok15 <theokroells@gmail.com>
2020-02-09 13:47:21 -0600

    Finished Shooting command

--- src/main/java/frc/command/autonomous/ShootCommand.java
+++ src/main/java/frc/command/autonomous/ShootCommand.java
@@ -1,21 +1,36 @@
 package frc.command.autonomous;
 
-// public class ShootCommand extends Command {
-//     private double shootingTime;
-//     private ShooterSubsystem shooterSubsystem;
-//     public ShootCommand (double shootingTime) {
-//         this.shootingTime = shootingTime;
-//         shooterSubsystem = ServiceLocator.get(ShooterSubsystem.class);
-//     }
-//     @Override
-//     public void init () {
-        
-//     }
-//     @Override
-//     public void execute () {
-//         shooterSubsystem.shootOut();
-//     }
-//     public boolean isFinished () {
-//         if (Timer.getFPGATimestamp() )
-//     }
-// }
+import edu.wpi.first.wpilibj.Timer;
+import frc.ServiceLocator;
+import frc.command.Command;
+import frc.subsystem.ShooterSubsystem;
+
+public class ShootCommand extends Command {
+     private double shootingTime;
+     private ShooterSubsystem shooterSubsystem;
+     public ShootCommand (double shootingTime) {
+         this.shootingTime = shootingTime;
+         shooterSubsystem = ServiceLocator.get(ShooterSubsystem.class);
+     }
+        double startTime;
+     @Override
+     public void init () {
+        startTime = Timer.getFPGATimestamp();
+     }
+     @Override
+     public void execute () {
+
+         shooterSubsystem.shootOut();
+     }
+     public boolean isFinished () {
+      if (Timer.getFPGATimestamp() - startTime > shootingTime){
+          return true;
+      }
+      else{
+          return false;
+      }
+     }
+     public void end() {
+        shooterSubsystem.stopShootOut();
+     }
+ }
