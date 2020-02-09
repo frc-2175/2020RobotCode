83165d5d37c42397b31ea438fdf6ee5f26f9c13a
theok15 <theokroells@gmail.com>
2020-02-09 15:02:00 -0600

    Fixed Alayna's errors :(

--- src/test/java/command/autonomous/SpinControlPanelCommandTest.java
+++ src/test/java/command/autonomous/SpinControlPanelCommandTest.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import frc.command.autonomous.SpinControlPanelCommand;
+import frc.command.autonomous.ControlPanelColorCommand;
 
 public class SpinControlPanelCommandTest {
    @Test
    public void testGetShortestDistance(){
-        assertEquals(-45, SpinControlPanelCommand.getShortestDistance("red", "green"), 0.001);
-        assertEquals(0, SpinControlPanelCommand.getShortestDistance("green", "yellow"), 0.001);
-        assertEquals(-45, SpinControlPanelCommand.getShortestDistance("green", "blue"), 0.001);
-        assertEquals(90, SpinControlPanelCommand.getShortestDistance("green", "green"), 0.001);
-        assertEquals(45, SpinControlPanelCommand.getShortestDistance("yellow", "blue"), 0.001);
+        assertEquals(-45, ControlPanelColorCommand.getShortestDistance("red", "green"), 0.001);
+        assertEquals(0, ControlPanelColorCommand.getShortestDistance("green", "yellow"), 0.001);
+        assertEquals(-45, ControlPanelColorCommand.getShortestDistance("green", "blue"), 0.001);
+        assertEquals(90, ControlPanelColorCommand.getShortestDistance("green", "green"), 0.001);
+        assertEquals(45, ControlPanelColorCommand.getShortestDistance("yellow", "blue"), 0.001);
    }
 }
