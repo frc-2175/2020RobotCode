21da93eef93b37ac660d9121f889d6b8a818c903
DriverStation <info@fightingcalculators.com>
2020-02-21 17:50:47 -0600

    change intake motors and speeds

--- src/main/java/frc/subsystem/IntakeSubsystem.java
+++ src/main/java/frc/subsystem/IntakeSubsystem.java
@@ -6,6 +6,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import frc.ServiceLocator;
+import frc.info.RobotInfo;
 
 public class IntakeSubsystem {
 
@@ -13,7 +15,9 @@ public class IntakeSubsystem {
     private final DoubleSolenoid intakePiston;
 
     public IntakeSubsystem() {
-        intakeMotor = new WPI_TalonSRX(10);
+        RobotInfo robotInfo = ServiceLocator.get(RobotInfo.class);
+
+        intakeMotor = robotInfo.pick(() -> new WPI_VictorSPX(1), () -> new WPI_TalonSRX(10));
         intakePiston = new DoubleSolenoid(0, 1);
         
         intakeMotor.setInverted(true);
@@ -23,7 +27,7 @@ public class IntakeSubsystem {
      * rolls intake in at full in
      */
     public void intakeRollIn() {
-        intakeMotor.set(-1);
+        intakeMotor.set(-.5);
     }
 
     public void stopIntake() {
@@ -42,7 +46,7 @@ public class IntakeSubsystem {
      * rolls intake out at full speed
      */
     public void intakeRollOut() {
-        intakeMotor.set(1);
+        intakeMotor.set(.5);
     }
 
     public void putOut() {
